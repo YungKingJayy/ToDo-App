a7eb5591ebca99f9a4d4e03b8a41fd887d123c6a
YungKingJayy <kalumcjethro@gmail.com>
2024-09-24 01:29:42 +0100

    feat: setup project with shadcn and Tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +10,36 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Light Mode
+        light: {
+          background: "#FFFFFF",
+          primaryText: "#333333",
+          secondaryText: "#666666",
+          accent: "#007BFF",
+          completedTask: "#28A745",
+          pendingTask: "#FFC107",
+          error: "#DC3545",
+        },
+        // Dark Mode
+        dark: {
+          background: "#121212",
+          primaryText: "#E0E0E0",
+          secondaryText: "#B3B3B3",
+          accent: "#1E90FF",
+          completedTask: "#34C759",
+          pendingTask: "#FFCA28",
+          error: "#FF3B30",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("tailwindcss-animate")],
 };
+
 export default config;
